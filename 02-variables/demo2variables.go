53469efcc54cb837d5fba7217a32946f58aa7f1d
Andry Hialy <ahr.dsc@gmail.com>
2025-10-22 11:57:35 -0400

    feat(02-variables): use variables within a function

---
+++ 02-variables/demo2variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		num         int    = 123
+		companyName string = "Qwerty"
+	)
+	fmt.Println(num)
+	fmt.Println(companyName)
+}
---
+++ 02-variables/go.mod
@@ -0,0 +1,3 @@
+module section2demo.com
+
+go 1.22.2
