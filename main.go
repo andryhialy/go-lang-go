3232d52642a75291e8941b6ea680c9a7eb6aa702
Andry Hialy <ahr.dsc@gmail.com>
2025-10-26 17:36:11 -0400

    fate: define book ticket logic

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -13,11 +13,25 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// Ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "John"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. Your will you receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 }
